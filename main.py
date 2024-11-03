9e88eb557715ed488fb2ff940ae4b9fd794b488c
ahmedsamyh <ahmedsamyh10@gmail.com>
2024-11-03 16:04:12 +0500

    A* works!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pprint
+from queue import Queue
 
 WIDTH  = 800
 HEIGHT = 800
@@ -19,19 +20,14 @@ def draw_square(pos, size, color, outline):
 
 class Cell:
     def __init__(self, size):
-        self.size = size
-        self.wall = False
-        self.on   = False
+        self.size  = size
 
     def draw(self, i, j):
         r = pygame.Rect(i*self.size[0], j*self.size[1], self.size[0], self.size[1])
-        color = "gray" if self.wall else "white"
         border = 1
-        if self.on or self.wall:
-            border = 0
-        pygame.draw.rect(screen, color, r, border)
+        pygame.draw.rect(screen, "white", r, border)
 
-CELL_SIZE = 32
+CELL_SIZE = 16
 assert WIDTH % CELL_SIZE == 0, f"CELL_SIZE {CELL_SIZE} is not divisble by WIDTH {WIDTH}!"
 assert HEIGHT % CELL_SIZE == 0, f"CELL_SIZE {CELL_SIZE} is not divisble by HEIGHT {HEIGHT}!"
 COLS = int(WIDTH / CELL_SIZE)
@@ -44,6 +40,8 @@ ROWS = int(HEIGHT / CELL_SIZE)
 cell = Cell((CELL_SIZE, CELL_SIZE))
 grid = []
 path = []
+flow_chart = []
+walls = {}
 
 prev_mouse_state = [False, False, False]
 mouse_pressed =    [False, False, False]
@@ -62,6 +60,9 @@ end_set_key_state_pressed   = False
 start_index = (0, 10)
 end_index =   (None, None)
 
+def clamp(value, mi, ma):
+    return min(ma, max(value, mi))
+
 for r in range(ROWS):
     row = []
     for c in range(COLS):
@@ -70,38 +71,53 @@ for r in range(ROWS):
 
 def get_cell_nbors(i, j):
     nbors = []
-    for n in range(-1, 1+1):
-        if n != 0:
-            if 0 <= i+n and i+n <= COLS-1 :
-                nbors.append((i+n, j))
-            if 0 <= j+n and j+n <= ROWS-1 :
-                nbors.append((i, j+n))
+    directions = [ (-1,  0), ( 1,  0),
+                   ( 0, -1), ( 0,  1),
+                   (-1, -1), (-1,  1),
+                   ( 1, -1), ( 1,  1) ]
+
+    for di, dj in directions:
+        ni = i + di
+        nj = j + dj
+        # Check if the neighbor is within bounds
+        if 0 <= ni < ROWS and 0 <= nj < COLS:
+            nbors.append((ni, nj))
     return nbors
 
 # Returns flow-chart to start from every cell
-def a_star(grid, start):
-    frontier = [start]
-    came_from = {}
+def a_star(grid, start, end):
+    # frontier = [start]
+    frontier = Queue()
+    frontier.put(start)
+    came_from = dict()
     came_from[start] = None
 
     # pprint.pp(f"Starting from {start}")
-    while len(frontier) > 0:
-        current = frontier.pop()
+    while not frontier.empty():
+        current = frontier.get()
+        if current in walls and walls[current]:
+            continue
+        if current == end:
+            break
         for n in get_cell_nbors(current[0], current[1]):
             if n not in came_from:
-                frontier.append(n)
+                frontier.put(n)
                 came_from[n] = current
                 # pprint.pp(f"Reached from {n}")
 
+    # assert len(came_from) == COLS*ROWS, "Couldn't calculate flow-chart of every cell!"
     return came_from
 
-def reconstruct_path(grid, start, end):
+def reconstruct_path(grid, flow_chart, start, end):
     current = end
     path = []
     while current != start:
-        current = a_star(grid, start)[current]
+        try:
+            current = flow_chart[current]
+        except Exception as e:
+            return path
         path.append(current)
-    pprint.pp(f"Calculated path! of {len(path)} cells")
+    # pprint.pp(f"Calculated path! of {len(path)} cells")
     return path
 
 def is_start_end_ready():
@@ -136,9 +152,12 @@ while not quit_game:
     pygame.draw.circle(screen, "red", (mi*CELL_SIZE + CELL_SIZE*0.5, mj*CELL_SIZE + CELL_SIZE*0.5), CELL_SIZE/8)
 
     if mouse_state[LEFT]:
-        hovering_cell.wall = True
+        walls[(mi, mj)] = True
     if mouse_state[RIGHT]:
-        hovering_cell.wall = False
+        walls[(mi, mj)] = False
+
+    #     for n in get_cell_nbors(mi, mj):
+    #         grid[n[0]][n[1]].color = "red"
 
     # Set start and end
     if start_set_key_state_pressed:
@@ -147,19 +166,31 @@ while not quit_game:
         end_index = (mi, mj)
 
     # TODO: Can check if the clicking index is not the current start/end index and not calculate path
-    if start_set_key_state_pressed or end_set_key_state_pressed and is_start_end_ready():
-        path = reconstruct_path(grid, start_index, end_index)
+    if mouse_state[LEFT] or mouse_state[RIGHT] or start_set_key_state_pressed or end_set_key_state_pressed:
+        if is_start_end_ready():
+            flow_chart = a_star(grid, start_index, end_index)
+            path = reconstruct_path(grid, flow_chart, start_index, end_index)
         # pprint.pp(path)
-
-    # "Turn on" the cells in path
-    for p in path:
-        grid[p[0]][p[1]].on = True
 
     # Draw grid
     for i, row in enumerate(grid):
         for j, cell in enumerate(row):
             cell.draw(i, j)
             cell.on = False
+
+    k = 10
+    # Draw flow-chart
+    for p in flow_chart:
+        draw_square((p[0]*CELL_SIZE, p[1]*CELL_SIZE), CELL_SIZE-2, (k, k, k, 255), 0)
+
+    # Highlight the cells in path
+    for p in path:
+        draw_square((p[0]*CELL_SIZE, p[1]*CELL_SIZE), CELL_SIZE-2, "yellow", 0)
+
+    # Draw walls
+    for p in walls:
+        if not walls[p]: continue
+        draw_square((p[0]*CELL_SIZE, p[1]*CELL_SIZE), CELL_SIZE-2, "gray", 0)
 
     # Draw start and end
     if start_index[0] != None and start_index[1] != None:
